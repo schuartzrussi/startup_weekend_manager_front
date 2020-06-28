afd9f4c8dc60921a6da8209894e113a18c608b82
Ruan Schuartz Russi <schuartzrussi@gmail.com>
2020-06-28 01:34:02 -0300

    timer >> legend

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import useInterval from '../../hooks/use_interval';
 import { Button } from '@material-ui/core';
@@ -9,15 +9,47 @@ const useStyles = makeStyles((theme) => ({
         textAlign: "center"
     },
 
-    text: {
+    textBig: {
+        display: "flex",
         fontSize: "60px"
-    }
+    },
+
+    textMedium: {
+        display: "flex",
+        fontSize: "40px"
+    },
+
+    textSmall: {
+        display: "flex",
+        fontSize: "30px"
+    },
+
+    legend: {
+        fontSize: "10px"
+    },
+
+    timeWithLegendSeparator: {
+        display: "inline-block"
+    },
+
+    legendContainer: {
+        display: "inline-block",
+    },
 }));
 
-export default function Timer({ seconds }) {
-    const [isRunning, setIsRunning] = useState(false);
-    const [currentTime, setCurrentTime] = useState(seconds * 1000);
+export default function Timer({ 
+        seconds, 
+        show_actions=true, 
+        auto_start=false,
+        show_legends=false }) {
+
+    const [isRunning, setIsRunning] = useState(auto_start);
+    const [currentTime, setCurrentTime] = useState(0);
     const classes = useStyles();
+
+    useEffect(() => {
+        setCurrentTime(seconds * 1000)
+    }, [])
 
     const start = function() {
         setIsRunning(true);
@@ -28,7 +60,16 @@ export default function Timer({ seconds }) {
     }
 
     const reset = function() {
-        setCurrentTime(seconds * 1000);
+        setCurrentTime(seconds * 1000)
+    }
+
+    const valueWithLegend = function(value, legend) {
+        return (
+            <div className={classes.legendContainer}>
+                <div>{value}</div>
+                <div className={classes.legend}>{legend}</div>
+            </div>
+        )
     }
 
     const onEnd = function() {
@@ -38,16 +79,84 @@ export default function Timer({ seconds }) {
 
     const renderTimer = function() {
         let milliseconds = parseInt((currentTime%1000));
-        let seconds = parseInt((currentTime/1000)%60);
-        let minutes = parseInt((currentTime/(1000*60))%60);
+        
+        let seconds = parseInt(Math.floor(currentTime / 1000));
+        let minutes = parseInt(Math.floor(seconds / 60));
+        let hours = parseInt(Math.floor(minutes / 60));
+        let days = parseInt(Math.floor(hours / 24));
+
+        seconds = parseInt(seconds % 60);
+        minutes = parseInt(minutes % 60);
+        hours = parseInt(hours % 24);
 
         seconds = seconds < 10 ? `0${seconds}` : seconds
         minutes = minutes < 10 ? `0${minutes}` : minutes
         milliseconds = milliseconds < 10 ? `0${milliseconds}` : milliseconds
         milliseconds = milliseconds.toString().substring(0, 2);
 
+        if (days > 0) {
+            days = days < 10 ? `0${days}` : days
+            hours = hours < 10 ? `0${hours}` : hours
+
+            if (show_legends) {
+                return (
+                    <div className={classes.textSmall}>
+                        {valueWithLegend(days, "dias")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(hours, "hrs")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(minutes, "min")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(seconds, "seg")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(milliseconds, "ms")}
+                    </div>
+                )
+            }
+
+            return (
+                <div className={classes.textSmall}>
+                    {days}:{hours}:{minutes}:{seconds}:{milliseconds}
+                </div>
+            )
+        } else if (hours > 0) {
+            hours = hours < 10 ? `0${hours}` : hours
+            
+            if (show_legends) {
+                return (
+                    <div className={classes.textMedium}>
+                        {valueWithLegend(hours, "hrs")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(minutes, "min")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(seconds, "seg")}
+                        <div className={classes.timeWithLegendSeparator}>:</div>
+                        {valueWithLegend(milliseconds, "ms")}
+                    </div>
+                )
+            } 
+
+            return (
+                <div className={classes.textMedium}>
+                    {hours}:{minutes}:{seconds}:{milliseconds}
+                </div>
+            )
+        }
+
+        if (show_legends) {
+            return (
+                <div className={classes.textBig}>
+                    {valueWithLegend(minutes, "min")}
+                    <div className={classes.timeWithLegendSeparator}>:</div>
+                    {valueWithLegend(seconds, "seg")}
+                    <div className={classes.timeWithLegendSeparator}>:</div>
+                    {valueWithLegend(milliseconds, "ms")}
+                </div>
+            )
+        }
+
         return (
-            <div className={classes.text}>{minutes}:{seconds}:{milliseconds}</div>
+            <div className={classes.textBig}>{minutes}:{seconds}:{milliseconds}</div>
         )
     }
 
@@ -63,12 +172,14 @@ export default function Timer({ seconds }) {
         <div className={classes.container}>
             {renderTimer()}
 
-            {isRunning ?
-                <Button onClick={() => stop()}>Parar</Button> :
-                <Button onClick={() => start()}>Iniciar</Button>
-            }
+            {show_actions && (<div>
+                {isRunning ?
+                    <Button onClick={() => stop()}>Parar</Button> :
+                    <Button onClick={() => start()}>Iniciar</Button>
+                }
 
-            <Button onClick={() => reset()}>Resetar</Button>
+                <Button onClick={() => reset()}>Resetar</Button>
+            </div>)}
         </div>
     )
 }
--- src/pages/user_home/index.js
+++ src/pages/user_home/index.js
@@ -1,16 +1,22 @@
 import React, { useState, useContext } from 'react';
-import { useHistory } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Logo from '../../images/logo.png';
 import { logout } from '../../services/api';
 import { authContext } from '../../contexts/AuthContext';
+import Timer from '../../components/timer';
 
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+
     menuButton: {
         marginRight: theme.spacing(2),
         [theme.breakpoints.up('sm')]: {
@@ -18,17 +24,78 @@ const useStyles = makeStyles((theme) => ({
         },
     },
 
+    toolbar: theme.mixins.toolbar,
+
     title: {
         flexGrow: 1,
+    },
+
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+
+    homeContainer: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        border: "0.1rem solid",
+        borderRadius: "20px",
+        borderColor: "#ececec",
+        backgroundColor: '#f7faf8',
+        padding: '20px'
+    },
+
+    logo: {
+        height: "80px",
+    },
+
+    timerContainer: {
+        textAlign: "center",
     }
 }));
 
 export default function UserHomePage() {
     const classes = useStyles();
     const { setAuthData } = useContext(authContext);
+    const [currentPhase, setCurrentPhase] = useState("NOT_STARTED");
+
+    const getPageContent = function() {
+        const getEventStartDate = function() {
+            let eventDate = new Date(2020, 5, 30, 10, 10, 10, 0);
+            let now = new Date();
+
+            if (eventDate > now) {
+                return (eventDate.getTime() - now.getTime()) / 1000;
+            }
+            return 0;
+        }
+        switch (currentPhase) {
+            case "NOT_STARTED":
+                return (
+                    <div className={classes.timerContainer}>
+                        <Typography variant="h6">
+                            Inicio em:
+                        </Typography>
+
+                        <Timer
+                            seconds={getEventStartDate()}
+                            show_actions={false}
+                            auto_start={true}
+                            show_legends={true}
+                        />
+                    </div>
+                )
+        }
+    }
+
+    const logoVisible = function() {
+        return currentPhase === "NOT_STARTED";
+    }
 
     return (
-        <div>
+        <div className={classes.root}>
             <CssBaseline />
             <AppBar position="fixed">
                 <Toolbar>
@@ -50,6 +117,19 @@ export default function UserHomePage() {
                         }}>Sair</Button>
                 </Toolbar>
             </AppBar>
+            
+            <div className={classes.content}>
+                <div className={classes.toolbar} />
+                <Container component="main" maxWidth="xs">
+
+                    <div className={classes.homeContainer}>
+                        {logoVisible() &&
+                            <img src={Logo} alt="logo" className={classes.logo} />}      
+
+                        {getPageContent()}
+                    </div>
+                </Container>
+            </div>
         </div>
     )
 }
